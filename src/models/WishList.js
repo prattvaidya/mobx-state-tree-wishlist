30a1777d21ca79259aedfe11d66a617c5b73e73b
prattvaidya <pratt.vaidya@gmail.com>
2021-04-18 14:34:01 -0700

    Remove items from the state tree

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -1,4 +1,4 @@
-import { types } from "mobx-state-tree";
+import { types, getParent, destroy } from "mobx-state-tree";
 
 export const WishListItem = types
   .model({
@@ -10,6 +10,7 @@ export const WishListItem = types
     changeName: (newName) => (self.name = newName),
     changePrice: (newPrice) => (self.price = newPrice),
     changeImage: (newImage) => (self.image = newImage),
+    remove: () => getParent(self, 2).remove(self), // two levels up in the state tree
   }));
 
 export const WishList = types
@@ -18,6 +19,12 @@ export const WishList = types
   })
   .actions((self) => ({
     add: (item) => self.items.push(item),
+    remove: (item) => {
+      destroy(item);
+
+      // alternate way
+      //self.items.splice(self.items.indexOf(item), 1)}
+    },
   }))
   .views((self) => ({
     get totalPrice() {
