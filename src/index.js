82ad23ce59e1363d2b81e23422ffb40a4be644df
prattvaidya <pratt.vaidya@gmail.com>
2021-04-18 14:58:55 -0700

    Store and fetch mobx state from local storage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { onSnapshot } from "mobx-state-tree";
 import "./assets/index.css";
 import App from "./components/App";
 import { WishList } from "./models/WishList";
 
-const wishList = WishList.create({
-  items: [
-    {
-      name: "The subtle art of not giving a f***",
-      price: 29,
-      // image:
-      //   "https://upload.wikimedia.org/wikipedia/en/b/bd/The_Subtle_Art_of_Not_Giving_a_F%2Ack_by_Mark_Manson_-_Book_Cover.png",
-    },
-    { name: "Finding Neverland", price: 43.3 },
-  ],
-});
+let initialState = {
+  items: [],
+  // [
+  //   {
+  //     name: "The subtle art of not giving a f***",
+  //     price: 29,
+  //     // image:
+  //     //   "https://upload.wikimedia.org/wikipedia/en/b/bd/The_Subtle_Art_of_Not_Giving_a_F%2Ack_by_Mark_Manson_-_Book_Cover.png",
+  //   },
+  //   { name: "Finding Neverland", price: 43.3 },
+  // ],
+};
+
+if (localStorage.getItem("wishlistapp")) {
+  const json = JSON.parse(localStorage.getItem("wishlistapp"));
+  if (WishList.is(json)) initialState = json;
+}
+
+const wishList = WishList.create(initialState);
+
+onSnapshot(wishList, (snapshot) =>
+  localStorage.setItem("wishlistapp", JSON.stringify(snapshot))
+);
 
 ReactDOM.render(
   <React.StrictMode>
